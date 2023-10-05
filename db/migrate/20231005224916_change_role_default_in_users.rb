d40695e1a6efa00bc37515a9f3e890d857b357ba
ProgramKingAbel <abelmorara254@gmail.com>
2023-10-06 01:50:37 +0300

    Change default role from User to user

---
+++ db/migrate/20231005224916_change_role_default_in_users.rb
@@ -0,0 +1,5 @@
+class ChangeRoleDefaultInUsers < ActiveRecord::Migration[7.0]
+  def change
+    change_column_default :users, :role, 'user'
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_05_205041) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_05_224916) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,7 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_05_205041) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: ""
-    t.string "role", default: "User"
+    t.string "role", default: "user"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
