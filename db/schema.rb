dd3c7c4ebb80e65f7ff151f8235210c27e714c40
ProgramKingAbel <abelmorara254@gmail.com>
2023-10-05 23:47:32 +0300

    Update Schema.rb with rooms table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_05_195255) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_05_204029) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.text "description"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "room_type"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: ""
