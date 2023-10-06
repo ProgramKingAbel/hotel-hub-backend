fda1a19c6c2a0e4676a1d1dbc5afa921e7391cc2
Enoisong <enobongitiaba988@gmail.com>
2023-10-06 08:39:42 -0700

    Add reservation methods

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,23 +1,58 @@
 class Api::V1::ReservationsController < ApplicationController
+  
   def index
     # Code to handle GET request to list reservations
     reservation = Reservation.all
     render json: reservation
   end
 
-  def create
-    # Code to handle POST request to create a reservation
-  end
+    def create
+      # Code to handle POST request to create a reservation
+      @reservation = Reservation.new(reservation_params)
+  
+      if @reservation.save
+        render json: { status: 'Success', message: 'Reservation created successfully' }, status: :created
+      else
+        render json: { error: 'Unable to create reservation.' }, status: :unprocessable_entity
+      end
+    end
+   
 
   def show
     # Code to handle GET request to retrieve a specific reservation
+    reservation = Reservation.find(params[:id])
+    render json: reservation
   end
 
   def update
     # Code to handle PUT/PATCH request to update a reservation
+    if @reservation.update(reservation_params)
+      render json: @reservation
+    else
+      render json: @reservation.errors, status: :unprocessable_entity
+    end
   end
+
 
   def destroy
     # Code to handle DELETE request to delete a reservation
+  
+      @reservation = Reservation.find_by(id: params[:id])
+      if @reservation
+        if @reservation.destroy
+          render json: { message: 'Reservation deleted successfully.' }
+        else
+          render json: { error: 'Unable to delete reservation.' }, status: :unprocessable_entity
+        end
+      else
+        render json: { error: 'Reservation not found.' }, status: :not_found
+      end
+    end
+    
+
+private
+
+def reservation_params
+  params.require(:reservation).permit(:check_in, :check_out, :room_id, :user_id)
   end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -9,9 +9,10 @@ class Reservation < ApplicationRecord
   validate :check_in_and_check_out_are_in_the_future
 
   def check_in_must_be_before_check_out
-    errors.add(:check_in, 'must be before check-out') if check_in >= check_out
-  end
-
+    if check_in.present? && check_out.present? && check_in >= check_out
+      errors.add(:check_in, 'must be before check-out')
+    end
+ 
   def no_reservation_overlap
     if Reservation.where(room_id:)
         .where('check_in < ? AND check_out > ?', check_out, check_in)
@@ -19,11 +20,11 @@ class Reservation < ApplicationRecord
         .exists?
       errors.add(:base, 'This room is already reserved during that time.')
     end
-  end
-
+  
   def check_in_and_check_out_are_in_the_future
     return unless check_in && check_out && (check_in < Date.today || check_out < Date.today)
-
     errors.add(:base, 'Reservations must be for future dates.')
+    end
   end
 end
+ 
