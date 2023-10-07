ba9ff920d7971047d9f45267f6f566e75cb6821f
Enoisong <enobongitiaba988@gmail.com>
2023-10-07 02:25:46 -0700

    Update Create Method

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -13,45 +13,10 @@ class Api::V1::ReservationsController < ApplicationController
       if @reservation.save
         render json: { status: 'Success', message: 'Reservation created successfully' }, status: :created
       else
+        puts @reservation.errors.full_messages
         render json: { error: 'Unable to create reservation.' }, status: :unprocessable_entity
       end
     end
    
 
-  def show
-    # Code to handle GET request to retrieve a specific reservation
-    reservation = Reservation.find(params[:id])
-    render json: reservation
-  end
-
-  def update
-    # Code to handle PUT/PATCH request to update a reservation
-    if @reservation.update(reservation_params)
-      render json: @reservation
-    else
-      render json: @reservation.errors, status: :unprocessable_entity
-    end
-  end
-
-
-  def destroy
-    # Code to handle DELETE request to delete a reservation
-      @reservation = Reservation.find_by(id: params[:id])
-      if @reservation
-        if @reservation.destroy
-          render json: { message: 'Reservation deleted successfully.' }
-        else
-          render json: { error: 'Unable to delete reservation.' }, status: :unprocessable_entity
-        end
-      else
-        render json: { error: 'Reservation not found.' }, status: :not_found
-      end
-    end
-    
-
-private
-
-def reservation_params
-  params.require(:reservation).permit(:check_in, :check_out, :room_id, :user_id)
-  end
-end
+ 
