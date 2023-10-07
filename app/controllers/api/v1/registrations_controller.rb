ae4949b6ffe300ec0fc475c7515ba740368de66d
sahlminkok <sahlminkok7@gmail.com>
2023-10-07 09:39:58 +0100

    Create registrations controller for devise

---
+++ app/controllers/api/v1/registrations_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
