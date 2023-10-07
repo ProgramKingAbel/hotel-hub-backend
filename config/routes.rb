2c67f9203836abbcf45566fb3a688be2ac462715
sahlminkok <sahlminkok7@gmail.com>
2023-10-07 16:31:54 +0100

    Add route for devise_token_auth in routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do  
+      mount_devise_token_auth_for 'User', at: 'auth'
       devise_for :users, controllers: {
-        registrations: 'api/v1/registrations',
-        sessions: 'api/v1/sessions'
-      }
+        registrations: 'api/v1/registrations'
+      }, skip: [:sessions]
       resources :rooms
       resources :reservations
     end
