60c77889e47ac8227127bf823e9d5260488f7b6a
sahlminkok <sahlminkok7@gmail.com>
2023-10-07 09:38:01 +0100

    Add registrations and sessions controllers for devise in routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do  
-      devise_for :users
+      devise_for :users, controllers: {
+        registrations: 'api/v1/registrations',
+        sessions: 'api/v1/sessions'
+      }
       resources :rooms
       resources :reservations
     end
