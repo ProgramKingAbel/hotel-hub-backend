fd5cf17f1e1cc3070c43d2a1e4495ea271674058
Enoisong <enobongitiaba988@gmail.com>
2023-10-07 02:28:08 -0700

    Modify Update Method

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -24,4 +24,39 @@ class Api::V1::ReservationsController < ApplicationController
       render json: reservation
     end
   
+    def update
+      # Code to handle PUT/PATCH request to update a reservation
+      if @reservation.update(reservation_params)
+        render json: @reservation
+      else
+        render json: @reservation.errors, status: :unprocessable_entity
+      end
+    end
+  
+  
+    def destroy
+      # Code to handle DELETE request to delete a reservation
+        @reservation = Reservation.find_by(id: params[:id])
+        if @reservation
+          if @reservation.destroy
+            render json: { message: 'Reservation deleted successfully.' }
+          else
+            render json: { error: 'Unable to delete reservation.' }, status: :unprocessable_entity
+          end
+        else
+          render json: { error: 'Reservation not found.' }, status: :not_found
+        end
+      end
+      
+  
+  private
+  
+  def reservation_params
+    params.require(:reservation).permit(:check_in, :check_out, :room_id, :user_id)
+    end
+  end
+  
+   
+
+ 
     
