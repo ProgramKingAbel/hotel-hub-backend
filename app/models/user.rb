ef050f2935f823143b27aa31b3878f24ac45b7db
sahlminkok <sahlminkok7@gmail.com>
2023-10-07 09:36:20 +0100

    Add uniqueness to the useraname of the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 
   has_many :reservations
 
-  validates :name, presence: true, length: { in: 3..25 }
+  validates :name, presence: true, length: { in: 3..25 }, uniqueness: true
 end
