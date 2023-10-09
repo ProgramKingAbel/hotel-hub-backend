6a68079fcc9a17357607b0ece24d1facec263338
ProgramKingAbel <abelmorara254@gmail.com>
2023-10-10 02:36:36 +0300

    add enum to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
   has_many :reservations
 
   validates :name, presence: true, length: { in: 3..25 }, uniqueness: true
+  enum role: { user: 'user', admin: 'admin' }
 end
