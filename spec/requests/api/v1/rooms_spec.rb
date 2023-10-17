803319393a0853dec98641709f1ffba6b2694a7c
Peter Dumbari <dummytwiz@gmail.com>
2023-10-17 11:01:55 +0100

    added test for invalid room post

--- spec/requests/api/v1/rooms_spec.rb
+++ spec/requests/api/v1/rooms_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Api::V1::Rooms', type: :request do
   let(:user) { create(:user) }
-
+  
   describe 'GET /api/v1/rooms' do
     it 'returns a JSON response with a list of rooms' do
       get "/api/v1/rooms"
@@ -30,7 +30,24 @@ RSpec.describe 'Api::V1::Rooms', type: :request do
       expect(JSON.parse(response.body)['message']).to eq('Room successfully created')
     end
 
-   
+    it 'returns unprocessable_entity status with invalid parameters' do
+      # Authenticate the user
+      sign_in user
+
+      # Send a POST request to the create action with invalid room parameters
+      post '/api/v1/rooms', params: {
+        room: {
+          name: '', # Invalid: name is required
+          price: 100,
+          description: 'A nice room',
+          room_type: 'Standard'
+        }
+      }
+
+      # Check the response
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(JSON.parse(response.body)['error']).to eq('Failed to create room')
+    end
   end
 
   describe 'GET /api/v1/rooms/:id' do
